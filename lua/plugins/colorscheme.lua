a6486b0d24f02d8ae243c314c1c0fe04ad19cef3
hg8116 <2011.harshgupta@gmail.com>
2024-03-06 15:16:09 +0530

    nvim config

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "sainnhe/sonokai",
+    priority = 1000,
+    config = function()
+      vim.g.sonokai_enable_italic = "1"
+      vim.g.sonokai_style = "atlantis"
+      vim.cmd.colorscheme("sonokai")
+    end,
+  },
+}
---
+++ lua/plugins/ui.lua
@@ -0,0 +1,54 @@
+return {
+  {
+    "nvimdev/dashboard-nvim",
+    enabled = false,
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+  },
+  -- dashboard
+  {
+    "goolord/alpha-nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+
+    config = function()
+      local alpha = require("alpha")
+      local dashboard = require("alpha.themes.startify")
+
+      dashboard.section.header.val = {
+        [[                                  __]],
+        [[     ___     ___    ___   __  __ /\_\    ___ ___]],
+        [[    / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\]],
+        [[   /\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \]],
+        [[   \ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+        [[    \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+      }
+
+      alpha.setup(dashboard.opts)
+    end,
+  },
+  -- buffer line
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+    },
+    opts = {
+      options = {
+        mode = "tabs",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
+    },
+  },
+}
