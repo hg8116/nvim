9305a6eb0ae7a31081cd4a111481fd1eeaefc396
hg8116 <2011.harshgupta@gmail.com>
2024-05-22 03:54:29 +0530

    noice.nvim

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -50,6 +50,23 @@ return {
 			})
 		end,
 	},
+	-- UI change for messages, cmdline and popupmenu
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- add any options here
+		},
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
+		},
+	},
+
 	-- buffer line
 	--  {
 	--    "akinsho/bufferline.nvim",
